610b423491e6eb21cb58294e7d7bed665843254d
nelesin <nikolay.elesin@mera.com>
2019-08-27 17:10:14 +0300

    converter

--- app/src/main/java/com/example/moneytracker/NewCostActivity.kt
+++ app/src/main/java/com/example/moneytracker/NewCostActivity.kt
@@ -8,7 +8,9 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
+import com.example.moneytracker.database.Cost
 import kotlinx.android.synthetic.main.activity_new_cost.*
+import org.joda.time.LocalDateTime
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -22,6 +24,9 @@ class NewCostActivity : AppCompatActivity() {
         editCostView = findViewById(R.id.edit_word)
 
         dateValue.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
+        val myFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        val formatter = DateTimeFormatter()
 
         var cal = Calendar.getInstance()
 
@@ -30,8 +35,6 @@ class NewCostActivity : AppCompatActivity() {
             cal.set(Calendar.MONTH, monthOfYear)
             cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
 
-            val myFormat = "dd.MM.yyyy"
-            val sdf = SimpleDateFormat(myFormat, Locale.US)
             dateValue.text = sdf.format(cal.time)
 
         }
@@ -49,7 +52,9 @@ class NewCostActivity : AppCompatActivity() {
             if (TextUtils.isEmpty(editCostView.text)) {
                 setResult(Activity.RESULT_CANCELED, replyIntent)
             } else {
-                val cost = editCostView.text.toString()
+
+
+                val cost = Cost(value=editCostView.toString().toFloat(),time = LocalDateTime.parse(dateValue.text,sdf)
 
                 replyIntent.putExtra(EXTRA_REPLY, cost)
                 setResult(Activity.RESULT_OK, replyIntent)
--- app/src/main/java/com/example/moneytracker/database/Converters.kt
+++ app/src/main/java/com/example/moneytracker/database/Converters.kt
@@ -4,18 +4,22 @@ import androidx.room.TypeConverter
 import java.sql.Timestamp
 import java.time.Instant
 import java.time.LocalDate
-import org.joda.time.LocalDateTime
+import java.time.LocalDateTime
 import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 class Converters {
     @TypeConverter
     fun fromTimestamp(value: String?): LocalDateTime? {
-        return if (value == null) null else LocalDateTime(value)
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+        return if (value == null) null else LocalDateTime.parse(value, formatter);
     }
 
     @TypeConverter
     fun dateToTimestamp(date: LocalDateTime?): String? {
-        return date?.toString()
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+        val formatDateTime = date?.format(formatter)
+        return formatDateTime
     }
 }
